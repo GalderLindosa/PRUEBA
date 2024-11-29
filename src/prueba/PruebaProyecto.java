96e02a1312c2b67f5872b5b765e21a677e737c2f
“Galder <Galderlindosa@gmail.com>
2024-11-29 12:00:39 +0100

    PRUEBA

---
+++ src/prueba/PruebaProyecto.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class PruebaProyecto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero=-1;
+		numero=numero+8;
+		System.out.println(numero);
+	}
+
+}
